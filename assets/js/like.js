fe8783d6345df531b2092dece6acfea724c63186
Emmanuel <Emmanuelsumeh@gmail.com>
2019-10-24 06:13:44 -0700

    updates

--- assets/js/like.js
+++ assets/js/like.js
@@ -1,9 +1,8 @@
-(function (d, e, s) {
-    if (d.getElementById("likebtn_wjs")) return;
-    a = d.createElement(e);
-    m = d.getElementsByTagName(e)[0];
-    a.async = 1;
-    a.id = "likebtn_wjs";
-    a.src = s;
-    m.parentNode.insertBefore(a, m)
-})(document, "script", "//w.likebtn.com/js/w/widget.js");
+// $(function(){
+// 	$(document).on('click', '.like-review', function(e) {
+// 		$(this).html('<i class="fa fa-heart" aria-hidden="true"></i> You liked this');
+//     $(this).children('.fa-heart').addClass('animate-like');
+    
+
+// 	});
+// });
